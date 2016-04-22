5c2544c1adf264309665271934305fa898ddfc6b
Kaison Fan <kfan2@uh.edu>
2016-04-22 22:13:07 +0000

    rental invoice methods

--- TEAM5OARS/application/models/DbTable/RentalInvoice.php
+++ TEAM5OARS/application/models/DbTable/RentalInvoice.php
@@ -6,5 +6,69 @@ class Application_Model_DbTable_RentalInvoice extends Zend_Db_Table_Abstract
     protected $_name = 'rental_invoice';
 
 
+    public function Rental_Invoice()
+    {
+
+    }
+    public function getInvoice_No()
+    {
+
+    }
+    public function setInvoice_No()
+    {
+
+    }
+    public function getInvoice_Date()
+    {
+
+    }
+    public function setInvoice_Date()
+    {
+
+    }
+    public function getInvoice_Due()
+    {
+
+    }
+    public function getInvoice_Duee()
+    {
+
+    }
+    public function getCC_No()
+    {
+
+    }
+    public function setCC_No()
+    {
+
+    }
+    public function getCC_Type()
+    {
+
+    }
+    public function setCC_Type()
+    {
+
+    }
+    public function getCC_Exp_Date()
+    {
+
+    }
+    public function setCC_Exp_Date()
+    {
+
+    }
+
+    public function getCC_Amt()
+    {
+
+    }
+
+    public function setCC_Amt()
+    {
+
+    }
+
+
 }
 
