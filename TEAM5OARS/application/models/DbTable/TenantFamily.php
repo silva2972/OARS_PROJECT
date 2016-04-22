0e9147e6f7fad1b6471d0b63caa17f4e3df5350f
Kaison Fan <kfan2@uh.edu>
2016-04-22 22:24:36 +0000

    tenant family methods

--- TEAM5OARS/application/models/DbTable/TenantFamily.php
+++ TEAM5OARS/application/models/DbTable/TenantFamily.php
@@ -6,5 +6,74 @@ class Application_Model_DbTable_TenantFamily extends Zend_Db_Table_Abstract
     protected $_name = 'tenant_family';
 
 
+    public function Tenant_Family()
+    {
+
+    }
+    public function getFamily_SS()
+    {
+
+    }
+    public function setFamily_SS()
+    {
+
+    }
+    public function getName()
+    {
+
+    }
+    public function setName()
+    {
+
+    }
+    public function getSpouse()
+    {
+
+    }
+    public function setSpouse()
+    {
+
+    }
+    public function getChild()
+    {
+
+    }
+    public function setChild()
+    {
+
+    }
+    public function getDivorced()
+    {
+
+    }
+    public function setDivorced()
+    {
+
+    }
+    public function getSingle()
+    {
+
+    }
+    public function setSingle()
+    {
+
+    }
+    public function getGender()
+    {
+
+    }
+    public function getGender()
+    {
+
+    }
+    public function getDOB()
+    {
+
+    }
+    public function setDOB()
+    {
+        
+    }
+
 }
 
