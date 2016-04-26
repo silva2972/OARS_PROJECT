a45b9eb4c9962d83957182e2467ec7c971695b3b
Luis Silva <silva2972@gmail.com>
2016-04-26 21:09:58 +0000

    Added log in to tenantintro and lookup agreement

--- TEAM5OARS/application/controllers/LookUpRentalAgreementController.php
+++ TEAM5OARS/application/controllers/LookUpRentalAgreementController.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once 'Zend/Controller/Action.php';
+require(dirname(__DIR__) . "/models/TenantAccountMapper.php");
 class LookUpRentalAgreementController extends Zend_Controller_Action
 {
 
@@ -10,35 +11,48 @@ class LookUpRentalAgreementController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        session_start();
+        $this->_aM = new TenantAccountMapper();
         $tenants = new Application_Model_DbTable_Tenant();
         $rentals = new Application_Model_DbTable_Rental();
         $apartments = new Application_Model_DbTable_Apartment();
-        $tenantss = 123456789;
-        if (isset($_POST["submit"])) {
-            $tenantss = $_POST["tenantss"];
+        $loggedIn = $this->_aM->LoggedIn();
+        
+        if (!$loggedIn)
+        {
+            header("location: " . $this->view->baseURL() . "/index");
+            exit();
         }
+        if ($loggedIn)
+        {
+            $username = $_SESSION['login_user'];
+            $tenant_info = $tenants->fetchRow(
+                $tenants->select()
+                ->where('username = ?',$username)
+            );
+            $tenantss = $tenant_info['tenant_ss'];
+            $tenant_info = $tenants->find($tenantss);
+            $rental_no = $tenant_info[0]->rental_no;
+            $rental_info = $rentals->find($rental_no);
+            $apt_no = $rental_info[0]->apt_no;
+            $apartment_info = $apartments->find($apt_no);
 
-        $tenant_info = $tenants->find($tenantss);
-        $rental_no = $tenant_info[0]->rental_no;
-        $rental_info = $rentals->find($rental_no);
-        $apt_no = $rental_info[0]->apt_no;
-        $apartment_info = $apartments->find($apt_no);
-
-        $apt_type = $apartment_info[0]->apt_type;
-        if($apt_type == 0) {
-            $type = "Studio";
-        } elseif ($apt_type == 1) {
-            $type = "One Bedroom";
-        } elseif ($apt_type == 2) {
+            $apt_type = $apartment_info[0]->apt_type;
+            if($apt_type == 0) {
+                $type = "Studio";
+            } elseif ($apt_type == 1) {
+                $type = "One Bedroom";
+            } elseif ($apt_type == 2) {
             $type = "Two Bedroom";
-        } else {
-            $type = "Three Bedroom";
-        }
+            } else {
+                $type = "Three Bedroom";
+            }
 
-        $this->view->tenant = $tenant_info;
-        $this->view->rental = $rental_info;
-        $this->view->apartment = $apartment_info;
-        $this->view->type = $type;
+            $this->view->tenant = $tenant_info;
+            $this->view->rental = $rental_info;
+            $this->view->apartment = $apartment_info;
+            $this->view->type = $type;
+        }
     }
 
 
--- TEAM5OARS/application/controllers/TenantIntroController.php
+++ TEAM5OARS/application/controllers/TenantIntroController.php
@@ -4,15 +4,16 @@ require_once 'Zend/Controller/Action.php';
 require(dirname(__DIR__) . "/models/TenantAccountMapper.php");
 class TenantIntroController extends Zend_Controller_Action
 {
-
     
     public function indexAction()
     {
         session_start();
         $this->_aM = new TenantAccountMapper();
-
+        $tenants = new Application_Model_DbTable_Tenant();
+        $tenants_autos = new Application_Model_DbTable_TenantAuto();
+        $tenants_family = new Application_Model_DbTable_TenantFamily();
         $loggedIn = $this->_aM->LoggedIn();
-
+        $username = $_SESSION['login_user'];
         //If not logged in we can't view this page
         if (!$loggedIn)
         {
@@ -24,7 +25,24 @@ class TenantIntroController extends Zend_Controller_Action
         {
             $name = $this->_aM->Name();
             $this->view->LoggedInView = "Welcome " . $name . " | <a href=" . $this->view->baseURL() . "/tenantaccount/logout>Logout</a>";
-            $this->view->role = $_SESSION['user_type'];
+            $username = $_SESSION['login_user'];
+            $tenant_info = $tenants->fetchRow(
+                $tenants->select()
+                ->where('username = ?',$username)
+                );
+            $tenantss = $tenant_info['tenant_ss'];
+            $tenant_autos = $tenants_autos->fetchAll(
+                $tenants_autos->select()
+                ->where('tenant_ss = ?',$tenantss)
+                );
+            $tenant_family = $tenants_family->fetchAll(
+                $tenants_family->select()
+                ->where('tenant_ss = ?', $tenantss)
+                );
+            
+            $this->view->tenant_info = $tenant_info;
+            $this->view->tenant_auto = $tenant_autos;
+            $this->view->tenant_family = $tenant_family;
             
         }
         else
