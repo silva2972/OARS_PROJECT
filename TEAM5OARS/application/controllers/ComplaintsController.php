e544931725580cfbb2a413c8ea4106f3113502d1
Nixon Mathew <nmathew33@gmail.com>
2016-04-27 04:23:01 +0000

    Completed UCUpdateComplaints, and UCAddComplaints

--- TEAM5OARS/application/controllers/ComplaintsController.php
+++ TEAM5OARS/application/controllers/ComplaintsController.php
@@ -8,7 +8,7 @@ class ComplaintsController extends Zend_Controller_Action
 {
 
     protected $isSupervisor = false;
-
+    protected $isTenant = false;
     protected $canEnter = false;
 
     protected $username = null;
@@ -27,17 +27,31 @@ class ComplaintsController extends Zend_Controller_Action
             $this->_aM = new TenantAccountMapper();
             $this->username = $_SESSION['login_user'];
             $this->canEnter = $this->_aM->LoggedIn();
+            if ($this->canEnter) {
+                $this->isTenant = true;
+            }
         }
     }
 
     public function indexAction()
     {
         if (!$this->isSupervisor) {
-            header("location: " . $this->view->baseURL() . "/index");
+            header("location: " . $this->view->baseURL());
             exit();
         }
+
         $complaints = new Application_Model_DbTable_ComplaintsView();
         $this->view->complaints = $complaints->fetchAll();
+
+        if (isset($_POST['update'])) {
+            $complaints = new Application_Model_DbTable_Complaints;
+            $id = ($_POST['id']);
+            $data = array (
+                'status' => 'F'
+            );
+            $complaints->update($data, 'complaint_no = ' . (int)$id);
+            header("location: " . $this->view->baseURL() . "/complaints");
+        }
     }
 
     public function updateAction()
@@ -47,12 +61,29 @@ class ComplaintsController extends Zend_Controller_Action
 
     public function addAction()
     {
-        if (!$this->canEnter) {
+        if (!$this->isTenant) {
             header("location: " . $this->view->baseURL() . "/index");
             exit();
         }
+        $complaints = new Application_Model_DbTable_Complaints();
+        $tenants = new Application_Model_DbTable_Tenant();
+        $rentals = new Application_Model_DbTable_Rental();
+        $apartments = new Application_Model_DbTable_Apartment();
+        $date = date('Y-m-d', time());
 
-        
+        $tenant = $tenants->fetchRow(
+                $tenants->select()
+                    ->where("username = ?", $this->username)
+                );
+        $rental = $rentals->fetchRow(
+                $rentals->select()
+                    ->where("rental_no = ?", $tenant->rental_no)
+                );
+        if (isset($_POST["submit"])) {
+            $content = $_POST["testimonial"];
+            $complaints->addComplaint($date, $content, $tenant['rental_no'], $rental['apt_no']);
+        }
+
     }
 
 
--- TEAM5OARS/application/models/DbTable/Complaints.php
+++ TEAM5OARS/application/models/DbTable/Complaints.php
@@ -10,7 +10,7 @@ class Application_Model_DbTable_Complaints extends Zend_Db_Table_Abstract
     private $rental_complaint;
     private $apt_complaint;
     private $status;
-    
+
     public function getComplaint_No() { return $complaint_no; }
     public function setComplaint_No($complaint_no) { $this->complaint_no = $complaint_no; }
     public function getComplaint_Date() { return $complaint_date; }
@@ -21,5 +21,14 @@ class Application_Model_DbTable_Complaints extends Zend_Db_Table_Abstract
     public function setApt_Complaint($apt_complaint) { $this->apt_complaint = $apt_complaint; }
     public function getStatus() { return $status; }
     public function setStatus($status) { $this->status = $status; }
-}
 
+    public function addComplaint($date, $apt_comp, $rental, $apt) {
+        $data = array(
+            'complaint_date' => $date,
+            'apt_complaint' => $apt_comp,
+            'rental_no' => $rental,
+            'apt_no' => $apt
+        );
+        $this->insert($data);
+    }
+}
