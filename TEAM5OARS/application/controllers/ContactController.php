8d6aefee58e05a65f1b5f17b6359c617358bf698
Luis Silva <silva2972@gmail.com>
2016-05-02 05:54:39 +0000

    Added more comments

--- TEAM5OARS/application/controllers/ContactController.php
+++ TEAM5OARS/application/controllers/ContactController.php
@@ -1,3 +1,7 @@
+<!--//Coded By: Kaisong Fan, Visak Varghese
+//Date Created: 04/20/2015
+//Date Approved: 04/25/2015
+//Approved By: Luis Silva-->
 <?php
 
 class ContactController extends Zend_Controller_Action
